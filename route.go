9b40ad226cf3ab086095c6d9a16d23c7ed179c60
xieran <enliset@qq.com>
2013-11-03 21:56:57 +0800

    xxx

--- route.go
+++ route.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net"
 	"os"
+	"io"
+	_ "io/ioutil"
 	"bufio"
 	"strings"
 	"fmt"
+	"bytes"
 	"flag"
 	"time"
 	"encoding/hex"
@@ -18,80 +21,41 @@ import (
 	"code.google.com/p/gopacket"
 )
 
-func openLiveHandle() (handle *pcap.Handle) {
-	var err error
-	if handle, err = pcap.OpenLive(gwiface, 1600, true, 0); err != nil {
-		panic(err)
-	}
-	if gfilter != "" {
-		log.Println("BPFFilter", gfilter)
-		if err = handle.SetBPFFilter(gfilter); err != nil {
-			panic(err)
+func findMyIpAndMac() (ip net.IP, mac net.HardwareAddr) {
+	var b bytes.Buffer
+	cmd := exec.Command("ip", "addr", "show", gwiface)
+	cmd.Stdout = &b
+	cmd.Run()
+
+	br := bufio.NewReader(&b)
+	for {
+		l, e := br.ReadString('\n')
+		if e != nil {
+			break
+		}
+		f := strings.Fields(l)
+		if len(f) < 2 {
+			continue
+		}
+		if f[0] == "link/ether" {
+			mac, _ = net.ParseMAC(f[1])
+		}
+		if f[0] == "inet" {
+			if i := strings.Index(f[1], "/"); i > 0 {
+				ip = make(net.IP, 4)
+				fmt.Sscanf(f[1][:i], "%d.%d.%d.%d", &ip[0], &ip[1], &ip[2], &ip[3])
+			}
 		}
 	}
+
+	if ip == nil {
+		panic("myip not found")
+	}
+	if len(mac) == 0 {
+		panic("mymac not found")
+	}
+
 	return
-}
-
-func openLiveSource() (src *gopacket.PacketSource, handle *pcap.Handle) {
-	handle = openLiveHandle()
-	src = gopacket.NewPacketSource(handle, handle.LinkType())
-	return
-}
-
-func openTestHandle() *pcap.Handle {
-	if handle, err := pcap.OpenOffline("test.pcap"); err != nil {
-		panic(err)
-	} else {
-		return handle
-	}
-}
-
-func liveLoop() {
-	if handle := openTestHandle(); handle != nil {
-		pktsrc := gopacket.NewPacketSource(handle, handle.LinkType())
-		log.Println("starts")
-		for pkt := range pktsrc.Packets() {
-			log.Println(pkt)
-		}
-	}
-}
-
-func testChangeFields() {
-	handle := openTestHandle()
-	pktsrc := gopacket.NewPacketSource(handle, handle.LinkType())
-	pkt := <-pktsrc.Packets()
-	log.Println(pkt)
-	tcpLayer := pkt.Layer(layers.LayerTypeTCP)
-	tcp, _ := tcpLayer.(*layers.TCP)
-	ipLayer := pkt.Layer(layers.LayerTypeIPv4)
-	ip, _ := ipLayer.(*layers.IPv4)
-	ip.SrcIP = net.IP{1,2,3,4}
-	ip.DstIP = net.IP{5,6,7,8}
-	tcp.SrcPort = layers.TCPPort(11)
-	tcp.DstPort = layers.TCPPort(22)
-	ethLayer := pkt.Layer(layers.LayerTypeEthernet)
-	eth, _ := ethLayer.(*layers.Ethernet)
-	eth.SrcMAC = net.HardwareAddr{1,2,3,4,5,6}
-	eth.DstMAC = net.HardwareAddr{6,3,1,2,3,6}
-
-	log.Printf("tcp %v\n", tcp)
-	log.Printf("ip %v\n", ip)
-	log.Printf("eth %v\n", eth)
-
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true,
-	}
-	tcp.SerializeTo(buf, opts)
-	ip.SerializeTo(buf, opts)
-	eth.SerializeTo(buf, opts)
-
-	handle2, _ := pcap.OpenLive("lo", 1600, true, 0)
-	handle2.WritePacketData(buf.Bytes())
-
-	bytes := buf.Bytes()
-	pkt2 := gopacket.NewPacket(bytes, layers.LinkTypeEthernet, gopacket.Default)
-	log.Println(pkt2)
 }
 
 func findGateway() (iface string, ip net.IP, mac net.HardwareAddr) {
@@ -146,76 +110,147 @@ func findGateway() (iface string, ip net.IP, mac net.HardwareAddr) {
 	return
 }
 
-type Conn struct {
-	Packets chan gopacket.Packet
-	conn net.Conn
+type Packet struct {
+	eth *layers.Ethernet
+	ip *layers.IPv4
+	tcp *layers.TCP
+	data []byte
 }
 
-func NewConn(conn net.Conn) *Conn {
-	t := &Conn{}
-	t.Packets = *new(chan gopacket.Packet)
-	t.conn = conn
-	go t.Run()
-	return t
-}
-
-func (t *Conn) Write(pkt []byte) (err error) {
+func WritePkt(w io.Writer, pkt []byte) (err error) {
+	if w == nil {
+		return
+	}
 	l := uint16(len(pkt))
-	binary.Write(t.conn, binary.BigEndian, l)
-	_, err = t.conn.Write(pkt)
+	binary.Write(w, binary.BigEndian, l)
+	_, err = w.Write(pkt)
 	return
 }
 
-func (t *Conn) Run() {
+func UpdatePkt(p *Packet) {
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		ComputeChecksums: true,
+		FixLengths: true,
+	}
+	p.tcp.SetNetworkLayerForChecksum(p.ip)
+	p.tcp.SerializeTo(buf, opts)
+	p.ip.SerializeTo(buf, opts)
+	p.eth.SerializeTo(buf, opts)
+	p.data = buf.Bytes()
+}
+
+func NewPkt(pbuf []byte) (p Packet, ok bool) {
+	pkt := gopacket.NewPacket(pbuf, layers.LinkTypeEthernet, gopacket.Default)
+	tcpLayer := pkt.Layer(layers.LayerTypeTCP)
+	p.tcp, _ = tcpLayer.(*layers.TCP)
+	ipLayer := pkt.Layer(layers.LayerTypeIPv4)
+	p.ip, _ = ipLayer.(*layers.IPv4)
+	ethLayer := pkt.Layer(layers.LayerTypeEthernet)
+	p.eth, _ = ethLayer.(*layers.Ethernet)
+	if !(p.tcp == nil || p.ip == nil || p.eth == nil) {
+		ok = true
+	}
+	p.data = pkt.Data()
+	return
+}
+
+func ConnLoop(conn net.Conn, cb func (pkt Packet)) {
 	var l uint16
 	var buf [1600]byte
 	for {
-		if err := binary.Read(t.conn, binary.BigEndian, &l); err != nil{
+		if err := binary.Read(conn, binary.BigEndian, &l); err != nil{
 			break
 		}
 		if l > 1600 {
 			break
 		}
 		pbuf := buf[:int(l)]
-		if _, err := t.conn.Read(pbuf); err != nil {
+		if _, err := conn.Read(pbuf); err != nil {
 			break
 		}
-		pkt := gopacket.NewPacket(pbuf, layers.LinkTypeEthernet, gopacket.Default)
-		t.Packets <- pkt
+		if p, ok := NewPkt(pbuf); ok {
+			cb(p)
+		}
 	}
-	t.conn.Close()
-	close(t.Packets)
+	log.Println("Close")
+	conn.Close()
+}
+
+func openLiveHandle() (handle *pcap.Handle) {
+	var err error
+	if handle, err = pcap.OpenLive(gwiface, 1600, true, 0); err != nil {
+		panic(err)
+	}
+	log.Println("Pcap", gwiface, "opened")
+	if gfilter != "" {
+		log.Println("BPFFilter", gfilter)
+		if err = handle.SetBPFFilter(gfilter); err != nil {
+			panic(err)
+		}
+	}
+	return
+}
+
+func nextPkt(h *pcap.Handle) (p Packet) {
+	for {
+		pbuf, _, err := h.ReadPacketData()
+		if err != nil {
+			panic(err)
+		}
+		var ok bool
+		if p, ok = NewPkt(pbuf); ok {
+			return
+		}
+	}
+	return
 }
 
 type TunnelClient struct {
 }
 
 func (t TunnelClient) Run() {
-	src, srcw := openLiveSource()
+	live := openLiveHandle()
+
+	log.Println("Client starts")
+
+	var conn net.Conn
+
+	doOut := func (p Packet) {
+		if bytes.Compare(p.eth.DstMAC, mymac) != 0 {
+			return
+		}
+		if myip.Equal(p.ip.DstIP) {
+			return
+		}
+		log.Println("socket >", p.ip.DstIP, len(p.data), "bytes")
+		WritePkt(conn, p.data)
+	}
+
+	doIn := func (p Packet) {
+		log.Println("socket <", p.ip.SrcIP, len(p.data), "bytes")
+		live.WritePacketData(p.data)
+	}
+
+	go func () {
+		var err error
+		for {
+			host := fmt.Sprintf("%s:%d", ghost, gport)
+			log.Println("Connecting", host)
+			conn, err = net.DialTimeout("tcp4", host, time.Second*10)
+			if err != nil {
+				log.Println(err)
+				time.Sleep(time.Second)
+				continue
+			}
+			log.Println("Connected", ghost)
+			ConnLoop(conn, doIn)
+			conn = nil
+		}
+	}()
 
 	for {
-		host := fmt.Sprintf("%s:%d", ghost, gport)
-		log.Println("Connecting", host)
-		conn, err := net.DialTimeout("tcp4", host, time.Second*10)
-		if err != nil {
-			log.Println(err)
-			time.Sleep(time.Second)
-			continue
-		}
-		log.Println("Connected", ghost)
-		c := NewConn(conn)
-		out: for {
-			select {
-			case in, ok := <-c.Packets:
-				srcw.WritePacketData(in.Data())
-				if !ok {
-					break out
-				}
-			case out := <-src.Packets():
-				c.Write(out.Data())
-			}
-		}
-		log.Println("Closed")
+		doOut(nextPkt(live))
 	}
 }
 
@@ -236,24 +271,6 @@ func NewNat() *Nat {
 	return t
 }
 
-func (t *Nat) Parse(pkt gopacket.Packet) (
-	eth *layers.Ethernet,
-	ip *layers.IPv4,
-	tcp *layers.TCP,
-	ok bool,
-) {
-	tcpLayer := pkt.Layer(layers.LayerTypeTCP)
-	tcp, _ = tcpLayer.(*layers.TCP)
-	ipLayer := pkt.Layer(layers.LayerTypeIPv4)
-	ip, _ = ipLayer.(*layers.IPv4)
-	ethLayer := pkt.Layer(layers.LayerTypeEthernet)
-	eth, _ = ethLayer.(*layers.Ethernet)
-	if tcp == nil || ip == nil || eth == nil {
-		ok = true
-	}
-	return
-}
-
 func (t *Nat) Hash(ip *layers.IPv4, tcp *layers.TCP) (h uint16) {
 	h += 3*uint16(ip.SrcIP[0]) + 23*uint16(ip.SrcIP[1]) +
 			 13*uint16(ip.SrcIP[2]) + 31*uint16(ip.SrcIP[3])
@@ -263,65 +280,58 @@ func (t *Nat) Hash(ip *layers.IPv4, tcp *layers.TCP) (h uint16) {
 	return
 }
 
-func (t *Nat) Set(pkt gopacket.Packet) (h uint16) {
-	return
-}
-
-func (t *Nat) In(pkt gopacket.Packet) (b []byte) {
-	eth, ip, tcp, ok := t.Parse(pkt)
-	if !ok {
-		return
-	}
-
-	k := tcp.DstPort
+func (t *Nat) Out(p Packet) (b []byte) {
+	k := p.tcp.DstPort
 	c, _ := t.table[k]
 	if c == nil {
 		return
 	}
 
-	eth.SrcMAC = c.DstMAC
-	eth.DstMAC = c.SrcMAC
+	p.eth.SrcMAC = c.DstMAC
+	p.eth.DstMAC = c.SrcMAC
 
-	ip.DstIP = c.SrcIP
-	tcp.DstPort = c.SrcPort
+	p.ip.DstIP = c.SrcIP
+	p.tcp.DstPort = c.SrcPort
 
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true,
-	}
-	tcp.SerializeTo(buf, opts)
-	ip.SerializeTo(buf, opts)
-	eth.SerializeTo(buf, opts)
-
-	if tcp.FIN || tcp.RST {
+	if p.tcp.FIN || p.tcp.RST {
+		log.Println("nat del", k)
 		delete(t.table, k)
 	} else {
 		c.Ts = time.Now()
 	}
 
-	return buf.Bytes()
+	UpdatePkt(&p)
+	log.Println("socket <", p.ip.SrcIP, len(p.data), "bytes")
+
+	return p.data
 }
 
-func (t *Nat) Out(pkt gopacket.Packet) (b []byte) {
-	eth, ip, tcp, ok := t.Parse(pkt)
-	if !ok {
-		return
+func (t *Nat) In(p Packet) (b []byte) {
+	//ioutil.WriteFile("/tmp/b.pkt", pkt.Data(), 0777)
+
+	h := layers.TCPPort(t.Hash(p.ip, p.tcp))
+	c, _ := t.table[h]
+	if p.tcp.SYN && c == nil {
+		log.Println("nat new", h)
+		c = &natConn{
+			Ts: time.Now(),
+			SrcPort: p.tcp.SrcPort,
+			SrcIP: p.ip.SrcIP,
+			SrcMAC: p.eth.SrcMAC,
+			DstMAC: p.eth.DstMAC,
+		}
+		t.table[h] = c
 	}
 
-	if tcp.SYN {
-		h := t.Set(pkt)
-	} else {
-	}
+	p.ip.SrcIP = myip
+	p.eth.SrcMAC = mymac
+	p.eth.DstMAC = gwmac
+	p.tcp.SrcPort = h
 
-	buf := gopacket.NewSerializeBuffer()
-	opts := gopacket.SerializeOptions{
-		ComputeChecksums: true,
-	}
-	tcp.SerializeTo(buf, opts)
-	ip.SerializeTo(buf, opts)
-	eth.SerializeTo(buf, opts)
+	UpdatePkt(&p)
+	log.Println("socket >", p.ip.DstIP, len(p.data), "bytes", "syn", p.tcp.SYN)
 
-	return buf.Bytes()
+	return p.data
 }
 
 func (t *Nat) Gc() {
@@ -331,7 +341,7 @@ type TunnelServer struct {
 }
 
 func (t TunnelServer) Run() {
-	src := openLiveSource()
+	live := openLiveHandle()
 
 	ln, err := net.Listen("tcp4", fmt.Sprintf(":%d", gport))
 	if err != nil {
@@ -339,32 +349,38 @@ func (t TunnelServer) Run() {
 	}
 	log.Println("Server starts: listening", gport)
 
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			panic(err)
+	var conn net.Conn
+
+	nat := NewNat()
+
+	doIn := func (pkt Packet) {
+		if b := nat.In(pkt); len(b) > 0 {
+			live.WritePacketData(b)
 		}
-		log.Println("Accepted from", conn.RemoteAddr())
+	}
 
-		c := NewConn(conn)
-		nat := NewNat()
+	doOut := func (pkt Packet) {
+		if b := nat.Out(pkt); len(b) > 0 {
+			WritePkt(conn, b)
+		}
+	}
 
-		out: for {
-			select {
-			case in, ok := <-c.Packets:
-				if !ok {
-					break out
-				}
-				if b := nat.In(in); len(b) > 0 {
-					handle.WritePacketData(b)
-				}
-			case out := <-src.Packets():
-				if b := nat.Out(out); len(b) > 0 {
-					t.Write(b)
-				}
+	go func () {
+		var err error
+		for {
+			conn, err = ln.Accept()
+			if err != nil {
+				log.Println(err)
+				continue
 			}
+			log.Println("Accepted from", conn.RemoteAddr())
+			ConnLoop(conn, doIn)
+			conn = nil
 		}
-		log.Println("Closed")
+	}()
+
+	for {
+		doOut(nextPkt(live))
 	}
 }
 
@@ -372,6 +388,8 @@ var (
 	gwiface string
 	gwip net.IP
 	gwmac net.HardwareAddr
+	myip net.IP
+	mymac net.HardwareAddr
 	gfilter string //"dst net 95.138.148.0 mask 255.255.255.0"
 	gport int
 	ghost string
@@ -379,17 +397,25 @@ var (
 
 func main() {
 	// "106.187.99.23:1988"
-	log.Println("starts")
 
+	var test bool
 	flag.StringVar(&ghost, "h", "", "tunnel server host ip")
 	flag.IntVar(&gport, "p", 9998, "tunnel server listen port")
 	flag.StringVar(&gfilter, "f", "", "tunnel pcap filter")
+	flag.BoolVar(&test, "t", false, "test")
 	flag.Parse()
 
 	gwiface, gwip, gwmac = findGateway()
 	log.Println("Found gateway", gwiface, gwip, gwmac)
 
-	if ghost != "" {
+	myip, mymac = findMyIpAndMac()
+	log.Println("Found my ip", myip, "mac", mymac)
+
+	if test {
+		return
+	}
+
+	if ghost == "" {
 		srv := TunnelServer{}
 		srv.Run()
 	} else {
